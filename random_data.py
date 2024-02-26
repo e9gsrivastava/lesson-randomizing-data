47b2f30a69ad99828d36db0ca53b6afd8d036224
e9gsrivastava <153479304+e9gsrivastava@users.noreply.github.com>
2024-02-26 13:01:28 +0530

    Update random_data.py

--- random_data.py
+++ random_data.py
@@ -12,7 +12,7 @@ def create_assets(p):
     today = datetime.now().date()
     assets = []
     for i in range(p):
-        asset_id = i+1
+        asset_id = i + 1
         purchase_date = today - timedelta(days=random.randint(420, 750))
         assets.append(
             {"id": asset_id, "purchase_date": purchase_date.strftime("%Y-%m-%d")}
@@ -24,17 +24,19 @@ def create_rentals(assets, q):
     """
     create rent for above assets
     """
-    rentals = []    
-    i=0
-    while len(rentals)<=q:
-        i+=1
-        asset=random.choice(assets)
+    rentals = []
+    for asset in assets:
         asset_id = asset["id"]
         purchase_date = datetime.strptime(asset["purchase_date"], "%Y-%m-%d").date()
-        rental_id = 101 + i
-        start_date = purchase_date + timedelta(days=random.randint(1, 365))
-        end_date = start_date + timedelta(days=random.randint(1, 30))
-        if start_date>=purchase_date and end_date>=start_date:
+
+        if len(assets) / q > 1:
+            r = len(assets) / q
+        else:
+            r = 1
+        for _ in range(r):
+            rental_id = len(rentals) + 1
+            start_date = purchase_date + timedelta(days=random.randint(1, 365))
+            end_date = start_date + timedelta(days=random.randint(1, 30))
             rentals.append(
                 {
                     "id": rental_id,
@@ -43,7 +45,11 @@ def create_rentals(assets, q):
                     "end_date": end_date.strftime("%Y-%m-%d"),
                 }
             )
-    return rentals
+    result = []
+    while len(result) <= q:
+        r = random.choice(rentals)
+        result.append(r)
+    return result
 
 
 if __name__ == "__main__":
