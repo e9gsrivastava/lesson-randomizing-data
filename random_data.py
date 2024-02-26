66393648f7a97f6c15afee47737898091e09c515
e9gsrivastava <153479304+e9gsrivastava@users.noreply.github.com>
2024-02-26 11:16:16 +0530

    Update random_data.py

--- random_data.py
+++ random_data.py
@@ -7,13 +7,13 @@ from datetime import datetime, timedelta
 
 def create_assets(p):
     """
-    to create assets
+    create random assets
     """
     today = datetime.now().date()
     assets = []
     for _ in range(p):
         asset_id = len(assets) + 1
-        purchase_date = today - timedelta(days=random.randint(500, 2000))
+        purchase_date = today - timedelta(days=random.randint(1, 365))
         assets.append(
             {"id": asset_id, "purchase_date": purchase_date.strftime("%Y-%m-%d")}
         )
@@ -22,36 +22,32 @@ def create_assets(p):
 
 def create_rentals(assets, q):
     """
-    to create rent
+    create rent for above assets
     """
     rentals = []
-    for _ in range(q):
-        rental_id = len(rentals) + 1
-        asset = random.choice(assets)
-        start_date = datetime.strptime(asset["purchase_date"], "%Y-%m-%d").date()
-        end_date = today = datetime.now().date()
-
-        while start_date >= end_date:
-            start_date = today - timedelta(days=random.randint(1, 365))
-            end_date = today - timedelta
-
-        rentals.append(
-            {
-                "id": rental_id,
-                "asset_id": asset["id"],
-                "start_date": start_date.strftime("%Y-%m-%d"),
-                "end_date": end_date.strftime("%Y-%m-%d"),
-            }
-        )
-
+    for asset in assets:
+        asset_id = asset["id"]
+        purchase_date = datetime.strptime(asset["purchase_date"], "%Y-%m-%d").date()
+        for _ in range(q):
+            rental_id = len(rentals) + 1
+            start_date = purchase_date + timedelta(days=random.randint(1, 365))
+            end_date = start_date + timedelta(days=random.randint(1, 30))
+            rentals.append(
+                {
+                    "id": rental_id,
+                    "asset_id": asset_id,
+                    "start_date": start_date.strftime("%Y-%m-%d"),
+                    "end_date": end_date.strftime("%Y-%m-%d"),
+                }
+            )
     return rentals
 
 
 if __name__ == "__main__":
-    all_assets = create_assets(50)
+    all_assets = create_assets(5)
+    all_rentals = create_rentals(all_assets, 3)
 
-    all_rentals = create_rentals(all_assets, 30)
-    # print("Assets:",assets)
-    # print(assets)
-
+    print("Assets:")
+    print(all_assets)
+    print("\nRentals:")
     print(all_rentals)
